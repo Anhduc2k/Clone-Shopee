9b51ac540d5abee2ad6d24642e045eb88c90e7cb
AnhDuc2k <khungkhip@gmail.com>
2021-08-23 13:24:03 +0700

    improve UX with global loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import 'normalize.css';
+import 'react-toastify/dist/ReactToastify.css';
 import 'src/assets/styles/global.scss';
 import Routes from './Routes';
-import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import Authorization from './components/Authorization/Authorization';
 import Loading from './components/Loading/Loading';
--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -6,13 +6,13 @@ import { useSelector } from 'react-redux';
 const StyledLoading = styled(LinearProgress)`
   position: fixed !important;
   top: 0;
-  left: 0;
   right: 0;
+  left: 0;
 `;
 
 export default function Loading() {
   const loading = useSelector(state => state.app.loading);
-  if (loading > 0) {
+  if (loading >= 1) {
     return <StyledLoading color="secondary" />;
   }
   return null;
